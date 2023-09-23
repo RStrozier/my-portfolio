a7d9e31deca071becf2780964ccd8dc3f6af7010
Rashida <rashida.strozier@gmail.com>
2023-09-23 17:55:37 -0400

    rearranged icons in message

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,7 +7,14 @@ const Welcome = () => {
       <div className=''>
         <div className='Entire-Welcome-App'>
         <div className="App">
-          <div className="Message container-md p-1 rounded-2">
+          <div className="Message container-sm p-1 rounded-2">
+          <div className="coding-icons">
+          <i class="fa-brands fa-react"></i>
+          <i class="fa-brands fa-css3-alt"></i>
+          <i class="fa-brands fa-html5"></i>
+          <i class="fa-brands fa-js"></i>
+          <i class="fa-brands fa-node"></i>
+          </div>
           <i class="fa-regular fa-hand"></i>
           <p className="Text-Full">
             Just like me, this website is a 
@@ -18,13 +25,6 @@ const Welcome = () => {
             -Rashida S.
             </p>
           </p>
-          <div className="coding-icons">
-          <i class="fa-brands fa-react"></i>
-          <i class="fa-brands fa-css3-alt"></i>
-          <i class="fa-brands fa-html5"></i>
-          <i class="fa-brands fa-js"></i>
-          <i class="fa-brands fa-node"></i>
-          </div>
           </div>
           <div className="Projects container-md">
            <Projects />
