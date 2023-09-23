a1e8a1f75d10a426c11528fc42eed4f876ad46d2
Rashida <rashida.strozier@gmail.com>
2023-09-23 19:57:22 -0400

    made the first container a component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,44 +1,16 @@
 import React from 'react';
 import '../App.css';
 import Projects from './Projects';
-import rashidaImage from '../imgs/rashida.jpg'
+import About from './About';
 
 const Welcome = () => {
     return (
       <div className=''>
-        <div className='Entire-Welcome-App'>
-          <div className="App">
-            <div className='message-signiture-container'>
-            <div className="Message container-sm p-1 rounded-2">
-             <div className="coding-icons">
-              <i class="fa-brands fa-react"></i>
-              <i class="fa-brands fa-css3-alt"></i>
-             <i class="fa-brands fa-html5"></i>
-             <i class="fa-brands fa-js"></i>
-             <i class="fa-brands fa-node"></i>
-             </div>
-          <i class="fa-regular fa-hand"></i>
-          <p className="Text-Full">
-            Just like me, this website is a 
-          <p className="Work-Text">
-              Work In Progress..
-          </p>
-          <p className="Signiture">
-            -Rashida S.
-          </p>
-          </p>
-          <div className='selfie-container'>
-            <img src={rashidaImage} alt="self" className="image-size" />
-          </div>
-          </div>
-            </div>
+          <About />
           <div className="Projects container-md">
            <Projects />
           </div>
         </div>  
-      </div>
-    </div>
       );
   };
-  
   export default Welcome;
