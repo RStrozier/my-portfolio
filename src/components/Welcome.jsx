770bb40709efa46529be67344aada01b8a9c04ae
Rashida <rashida.strozier@gmail.com>
2023-09-23 11:40:52 -0400

    added icons

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -15,6 +15,13 @@ const Welcome = () => {
             -Rashida S.
             </p>
           </p>
+          <div className="coding-icons">
+          <i class="fa-brands fa-react"></i>
+          <i class="fa-brands fa-css3-alt"></i>
+          <i class="fa-brands fa-html5"></i>
+          <i class="fa-brands fa-js"></i>
+          <i class="fa-brands fa-node"></i>
+          </div>
           </div>
         </div>
       );
