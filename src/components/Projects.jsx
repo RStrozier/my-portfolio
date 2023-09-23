9ed6d5a4e00dde49d852866dbbd09d1456070762
Rashida <rashida.strozier@gmail.com>
2023-09-23 19:24:59 -0400

    made mobile view changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,14 +13,14 @@ const Projects = () => {
   };
 
   return (
-    <div className="projects-container rounded-2">
+    <div className="projects-container container-md rounded-2">
       <h2 className='project-text rounded-1'>Check out my current projects:</h2>
       <div className="projects container-md rounded-2">
         <div className='love-project container-sm images-container rounded-3'>
           <img src={loveImage} alt="img-1" className="image-size" />
           <div className="caption rounded-2">
             <h2>Love Compatibility</h2>
-            <p>Use a custom cursor to type in a partner's name and determine love using the compatibility API.</p>
+            <p className='project-description'>Use a custom cursor to type in a partner's name and determine love using the compatibility API.</p>
             <button className='button rounded-2' onClick={handleClickLove}>Try It!</button>
           </div>
         </div>
@@ -28,7 +28,7 @@ const Projects = () => {
           <img src={aframeImage} alt="img-2" className="image-size" />
           <div className="caption rounded-2">
             <h2>A-Frame Environment</h2>
-            <p>Start on a block with a spinning circle end goal. In VR HTML compatible.</p>
+            <p className='project-description'>Start on a block with a spinning circle end goal. In VR HTML compatible.</p>
             <button className='button rounded-2' onClick={handleClickAframe}>Try It!</button>
           </div>
         </div>
