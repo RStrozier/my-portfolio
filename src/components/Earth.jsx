2c8ea5a9ebd5c31ce1d7b6ae552ed34b6365579c
Rashida <rashida.strozier@gmail.com>
2023-09-23 22:14:36 -0400

    adjusted mobile size fonts on earth

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -3,7 +3,12 @@ import '../Earth.css';
 
 const Earth = () => {
   return (
-    <div className="earth-container container-lg rounded-2 fade-in">
+    <div className="earth-container container-lg rounded-2">
+        <div className='create-container rounded-3'>
+            <p className='create-text'>Create <b>anything</b> you can 
+            <br />
+            <i>imagine</i></p>
+        </div>
       <div className="earth"></div>
     </div>
   );
