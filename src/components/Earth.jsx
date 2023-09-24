b85f79f469078ec3b80c59ef34e1be71ecc5ae48
Rashida <rashida.strozier@gmail.com>
2023-09-23 21:58:22 -0400

    changed Earth mobile view

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -3,7 +3,7 @@ import '../Earth.css';
 
 const Earth = () => {
   return (
-    <div className="earth-container container-lg rounded-2">
+    <div className="earth-container container-lg rounded-2 fade-in">
       <div className="earth"></div>
     </div>
   );
