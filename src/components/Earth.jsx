4c6d0412b0e98ff5f30d21526cc53d227e82157a
Rashida <rashida.strozier@gmail.com>
2023-09-24 09:22:10 -0400

    added box shadows and edited mobile earth

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -3,13 +3,16 @@ import '../Earth.css';
 
 const Earth = () => {
   return (
-    <div className="earth-container container-lg rounded-2">
+    <div className='black-earth-container rounded-2'>
+        <div className="earth-container container-md rounded-3">
         <div className='create-container rounded-3'>
-            <p className='create-text'>Create <b>anything</b> you can 
-            <br />
-            <i>imagine</i></p>
+            <p className='create-text'>The Earth is a canvas, 
+            <br /><i>and we are the artists. </i>
+            <br />Let's paint a masterpiece of love, 
+            <br />compassion, and sustainability.</p>
         </div>
       <div className="earth"></div>
+    </div>
     </div>
   );
 };
